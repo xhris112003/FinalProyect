29266aed29f6ae23f7dd40bc0be9d61b7e8ee8e8
Natalie <ngomez.cep@gmail.com>
2023-05-16 01:57:58 +0200

    Añadidas fotos pág cuestionario

--- resources/views/cuestionario.blade.php
+++ resources/views/cuestionario.blade.php
@@ -92,10 +92,12 @@
 
 </div>
 </div>
-
-    <div id="cargando" class="oculto" style="display: none;">
-        <img src="https://media.tenor.com/joLYNfFQGDgAAAAC/loading.gif" alt="Cargando...">
-    </div>
+<div id="cargando" class="oculto" style="display: none;">
+    
+<img src="https://media.tenor.com/joLYNfFQGDgAAAAC/loading.gif" alt="Cargando...">
+</div>
+<div id="fotos-container" style="display: none;">
+<h2>Últimos estrenos</h2> </div>
     <div id="caja-resultados">
         <h1 style="display: none;" id="h2">Tus peliculas recomendadas</h1>
         <ul id="lista-resultados"></ul>
