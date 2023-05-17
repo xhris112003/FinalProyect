e76151864979fbf415d2ea2e242f170c0c0b38ab
delafuentea2 <a.delafuente1dawnuria2021@gmail.com>
2023-05-17 21:09:14 +0200

    Like Revisar + AcercaDe

--- resources/views/acercade.blade.php
+++ resources/views/acercade.blade.php
@@ -1,27 +1,21 @@
+@extends('layouts.homeTpl')
 <!DOCTYPE html>
 <html>
 <head>
     <title>Acerca de/Sobre nosotros</title>
     <style>
-        body {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-        }
 
         .team-member {
             display: flex;
             flex-direction: column;
             align-items: center;
             text-align: center;
-            margin-bottom: 20px;
+            margin: 10px;
         }
 
         .team-member img {
-            width: 150px;
-            height: 150px;
+            width: 100px;
+            height: 100px;
             border-radius: 50%;
             margin-bottom: 10px;
         }
@@ -35,14 +29,48 @@
             font-size: 16px;
         }
     </style>
+
+
 </head>
 <body>
+    <div class="home">
+        <nav class="home-inner">
+          <div class="frame-wrapper">
+            <div class="mm-parent">
+              <div class="mm"><a href="{{route('/')}}"> <img src="img/logoMMOscuro.png" alt="MDN" id="logo"> </a></div>
+              <div class="pelculas-parent">
+                <a class="pelculas" href="{{route('/')}}">Películas</a>
+                <a class="acerca-de" href="{{route('acercade')}}">Acerca de </a>
+                <a class="series" href="{{route('series')}}">Series</a>
+              </div>
+              <div class="group-parent">
+                <button onclick="window.location.href='/login'" class="rectangle-parent">
+                  <div class="rectangle"></div>
+                  <div class="iniciar-sesin-wrapper">
+                    <div class="iniciar-sesin">INICIAR SESIÓN</div>
+                  </div>
+                </button>
+                <button onclick="window.location.href='/register'" class="rectangle-group">
+                  <div class="rectangle1"></div>
+                  <div class="registrarse-wrapper">
+                    <div class="registrarse">Registrarse</div>
+                  </div>
+                </button>
+              </div>
+              <button class="image-3-wrapper">
+                <img class="image-3-icon" alt="" src="https://i.postimg.cc/J0wfG2Vg/icons8-men-30.png" />
+              </button>
+            </div>
+          </div>
+        </nav>
+    </div>
+
     <h1>Acerca de</h1>
 
     <div class="team-member">
         <img src="{{ asset('img/icon.jpg') }}" alt="Foto del integrante 1">
         <div class="team-member-details">
-            <h2>Cristian Carrasco</h2>
+            <h3>Cristian Carrasco</h3>
             <p>Para nada ha hecho casi todo el trabajo</p>
         </div>
     </div>
@@ -50,22 +78,22 @@
     <div class="team-member">
         <img src="{{ asset('img/icon.jpg') }}" alt="Foto del integrante 2">
         <div class="team-member-details">
-            <h2>Natalie Gomez</h2>
-            <p>Me gusta comer, los gatos y ¡¡Adri, trabaja!! >:(</p>
+            <h3>Natalie Gomez</h3>
+            <p>Me gusta comer, los gatos y ¡¡Adri, trabaja!! >:|</p>
         </div>
     </div>
 
     <div class="team-member">
         <img src="{{ asset('img/icon.jpg') }}" alt="Foto del integrante 3">
         <div class="team-member-details">
-            <h2>Adrian De la Fuente</h2>
+            <h3>Adrian De la Fuente</h3>
             <p>Condecorado oficial por hacer el puto vago</p>
         </div>
     </div>
     <div class="team-member">
         <img src="{{ asset('img/chatgpt.png') }}" alt="Foto del integrante 3">
         <div class="team-member-details">
-            <h2>CHATGPT</h2>
+            <h3>CHATGPT</h3>
             <p>El Becario</p>
         </div>
     </div>
@@ -79,7 +107,7 @@
             member.addEventListener('click', () => {
                 // Obtener el elemento de detalles del miembro
                 const memberDetails = member.querySelector('.team-member-details');
-                
+
                 // Alternar la visibilidad de los detalles
                 memberDetails.classList.toggle('show-details');
             });
